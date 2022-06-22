fe3becac3bd69d7629185de7be00e968f067d796
Famouse <famouse_ru@mail.ru>
2022-06-22 13:27:29 +0300

    Create BaseRepository

---
+++ EquipmentMaintenance.Infrastructure/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using EquipmentMaintenance.Core.Interfaces.Entities;
+using EquipmentMaintenance.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentMaintenance.Infrastructure.Data.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T>
+        where T : class, IEntity
+    {
+        protected EquipmentMaintenanceContext DbContext { get; }
+        protected CsvDbSet<T> CurrentDbSet { get; }
+
+        public BaseRepository(EquipmentMaintenanceContext dbContext, CsvDbSet<T> dbSet)
+        {
+            DbContext = dbContext;
+            CurrentDbSet = dbSet;
+        }
+
+        public List<T> GetAll()
+        {
+            return CurrentDbSet.ToList();
+        }
+
+        public bool Insert(T equipment)
+        {
+            CurrentDbSet.Add(equipment);
+
+            return true;
+        }
+
+        public void SaveChanges() => CurrentDbSet.SaveChanges();
+
+        public async Task SaveChangesAcync() => await CurrentDbSet.SaveChangesAcync();
+    }
+}
