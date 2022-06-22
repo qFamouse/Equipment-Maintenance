028650bcc0f67586cee155034e224536047a37bb
Famouse <famouse_ru@mail.ru>
2022-06-22 13:26:25 +0300

    Create IEquipmentRepository

---
+++ EquipmentMaintenance.Core/Interfaces/Repositories/IEquipmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using EquipmentMaintenance.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentMaintenance.Core.Interfaces.Repositories
+{
+    public interface IEquipmentRepository : IBaseRepository<Equipment>
+    {
+
+    }
+}
