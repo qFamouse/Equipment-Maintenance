ef6335c32edc5d625909bd3ff62a1724f2c6fd96
Famouse <famouse_ru@mail.ru>
2022-06-22 13:26:46 +0300

    Create EquipmentService

---
+++ EquipmentMaintenance.Core/Services/EquipmentService.cs
@@ -0,0 +1,26 @@
+﻿using EquipmentMaintenance.Core.Entities;
+using EquipmentMaintenance.Core.Interfaces.Repositories;
+using EquipmentMaintenance.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentMaintenance.Core.Services
+{
+    public class EquipmentService : IEquipmentService
+    {
+        private readonly IEquipmentRepository equipmentRepository;
+
+        public EquipmentService(IEquipmentRepository equipmentRepository)
+        {
+            this.equipmentRepository = equipmentRepository;
+        }
+
+        public List<Equipment> GetAll()
+        {
+            return equipmentRepository.GetAll();
+        }
+    }
+}
