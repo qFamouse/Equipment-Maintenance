570bf7467e0f27040aaed0fec13e3c0e08cf0e36
Famouse <famouse_ru@mail.ru>
2022-06-22 13:22:47 +0300

    Create IBaseRepository

---
+++ EquipmentMaintenance.Core/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentMaintenance.Core.Interfaces.Repositories
+{
+    public interface IBaseRepository<T>
+    {
+        List<T> GetAll();
+        bool Insert(T entity);
+    }
+}
