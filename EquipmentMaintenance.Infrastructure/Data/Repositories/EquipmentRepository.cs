04ffea5ebb7025cae306e4f3daf6326e449414d9
Famouse <famouse_ru@mail.ru>
2022-06-22 13:27:17 +0300

    Attach IEquipmentRepository to EquipmentRepository

---
+++ EquipmentMaintenance.Infrastructure/Data/Repositories/EquipmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using EquipmentMaintenance.Core.Entities;
+using EquipmentMaintenance.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentMaintenance.Infrastructure.Data.Repositories
+{
+    public class EquipmentRepository : BaseRepository<Equipment>, IEquipmentRepository 
+    {
+        public EquipmentRepository(EquipmentMaintenanceContext context) : base(context, context.Equipments) { }
+    }
+}
