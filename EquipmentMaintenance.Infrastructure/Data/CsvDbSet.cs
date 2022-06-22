ff0df35e409ee808e6769b09cf0a04b3eaef4dff
Famouse <famouse_ru@mail.ru>
2022-06-22 13:29:10 +0300

    CsvDbSet now extends IList Specifying that T is a class, IEntity Create decision for IDs;

--- EquipmentMaintenance.Infrastructure/Data/CsvDbSet.cs
+++ EquipmentMaintenance.Infrastructure/Data/CsvDbSet.cs
@@ -1,5 +1,6 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
+using EquipmentMaintenance.Core.Interfaces.Entities;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,12 +12,21 @@ using System.Threading.Tasks;
 
 namespace EquipmentMaintenance.Infrastructure.Data
 {
-    public class CsvDbSet<T>
+    public class CsvDbSet<T> : IList<T>
+        where T : class, IEntity
     {
-        public IList<T> List { get; set; }
+        private IList<T> List { get; set; }
+
+        private int _id;
 
         private string _path;
         private CsvConfiguration Configuration { get; }
+
+        public int Count => List.Count;
+
+        public bool IsReadOnly => List.IsReadOnly;
+
+        public T this[int index] { get => List[index]; set => List[index] = value; }
 
         public CsvDbSet(string path)
         {
@@ -35,6 +45,8 @@ namespace EquipmentMaintenance.Infrastructure.Data
                 var records = csv.GetRecords<T>();
                 List = records.ToList();
             }
+
+            _id = List.Count == 0 ? 1 : List.Max(x => x.Id) + 1;
         }
 
         public void SaveChanges()
@@ -49,7 +61,7 @@ namespace EquipmentMaintenance.Infrastructure.Data
             }
         }
 
-        public async void SaveChangesAcync()
+        public async Task SaveChangesAcync()
         {
             using (var writer = new StreamWriter(_path))
             using (var csv = new CsvWriter(writer, Configuration))
@@ -60,5 +72,33 @@ namespace EquipmentMaintenance.Infrastructure.Data
                 await csv.WriteRecordsAsync(List);
             }
         }
+
+        public int IndexOf(T item) => List.IndexOf(item);
+
+        public void Insert(int index, T item)
+        {
+            item.Id = _id++;
+            List.Insert(index, item);
+        }
+
+        public void RemoveAt(int index) => List.RemoveAt(index);
+
+        public void Add(T item)
+        {
+            item.Id = _id++;
+            List.Add( item);
+        }
+
+        public void Clear() => List.Clear();
+
+        public bool Contains(T item) => List.Contains(item);
+
+        public void CopyTo(T[] array, int arrayIndex) => List.CopyTo(array, arrayIndex);
+
+        public bool Remove(T item) => List.Remove(item);
+
+        public IEnumerator<T> GetEnumerator() => List.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => (IEnumerator)this.GetEnumerator();
     }
 }
